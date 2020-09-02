2487c31bb83b500f4c7cd83185bf1add6e8b2d70
Kai Armer <kai_armer@hotmail.com>
2020-09-02 21:44:10 +0100

    End of chapter 2

--- src/main/kotlin/com/mkodo/kentbeck/Dollar.kt
+++ src/main/kotlin/com/mkodo/kentbeck/Dollar.kt
@@ -2,8 +2,8 @@ package com.mkodo.kentbeck
 
 class Dollar(var amount: Int) {
 
-    fun times(multiplier: Int) {
-        this.amount *= multiplier
+    fun times(multiplier: Int): Dollar {
+        return Dollar(amount * multiplier)
     }
 
 }
